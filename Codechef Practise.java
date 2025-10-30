f99e416f5d1ca21edcade8998b7618c40553af2a
mohammadsiddik658-web <mohammadsiddik658@gmail.com>
2025-10-30 10:56:35 +0530

    Add files via upload

---
+++ Codechef Practise.java
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/WAPEN
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		System.out.print((m*10)+n);
+
+	}
+}
